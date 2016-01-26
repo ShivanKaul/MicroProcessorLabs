a1cc8c412a3480f32f3fab3470fbb5c17ca89f2f
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-01-26 15:25:44 -0500

    shivan's annoyed

---
+++ kalman.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+
+}
+
+int Kalmanfilter_C (float* InputArray, float* OutputArray, kalman_state* kstate, int Length) {
+					printf("o/\n");
+}
