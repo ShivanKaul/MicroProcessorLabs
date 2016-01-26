5a0eb32f7c64367d02d00c692b14652d3350ce93
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-01-26 15:57:52 -0500

    kalman filter

--- Lab1_STM32F4Cube_Base_project/Sources/main.c
+++ Lab1_STM32F4Cube_Base_project/Sources/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include "arm_math.h"
+//#include "arm_math.h"
 
-int main()
-{
 
-	return 0;
+typedef struct {
+	float x, p, q, r, k;
+} kalman_state;
+
+int main(){
+	float input[]={10.0,11.0,12.0,13.0,14.0}, output[5];
+	int i =0;
+	for (;i<5; i++ ){
+		output[i] = input[i]+1.0f;
+	}
+}
+int Kalmanfilter_C (float* InputArray, float* OutputArray, kalman_state* kstate, int Length) {
+					printf("o/\n");
+	return 1;
 }
