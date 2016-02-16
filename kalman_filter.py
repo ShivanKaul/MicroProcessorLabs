2bbe82f692cf177bcfcdf358c151b48a611f344a
ShivanKaul <shivankaul.1993@gmail.com>
2016-02-16 13:56:00 -0500

    Kalman filter

---
+++ kalman_filter.py
@@ -0,0 +1,29 @@
+class KalmanFilter(object):
+	q = 0.0
+	r = 0.0
+	x = 0.0
+	p = 0.0
+	k = 0.0
+
+	def __init__(self, q=0.1, r=0.1, p=0.1, k=0.0, initial_value=0.0):
+		self.q = q
+		self.r = r
+		self.p = p
+		self.x = initial_value
+
+	def update(self, measurement):
+		self.p = self.p + self.q
+		self.k = self.p / (self.p + self.r)
+		self.x = self.x + self.k * (measurement - self.x)
+		self.p = (1 - self.k) * self.p
+
+		return self.x
+
+
+k = KalmanFilter()
+l1 = [10.0, 11.0, 12.0, 13.0, 14.0, 15.0]
+l2 = []
+for l in l1:
+	l2.append(k.update(l))
+
+print l2
