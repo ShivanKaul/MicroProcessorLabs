ac5c869bc682b177a28f521c793e81e46725b0c1
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-02-18 15:48:43 -0500

    SHIVAN IS ANNOYING

--- plotting.py
+++ plotting.py
@@ -1,11 +1,11 @@
-import ohai.samples as samples
+from  ohai import samples
 import matplotlib.pyplot as plt 
-import kalmanfilter.KalmanFilter
+from  kalman_filter import KalmanFilter
 
 x = xrange(len(samples))
-kf = KalmanFilter()
-
-filtered = [kf.update(i) for i in samples]
-
-plt.plot(x,samples,x,filtered)
+kf = KalmanFilter(p=0.1,r=300,q=0.1,k=0,initial_value=samples[0])
+kf2 = KalmanFilter(p=10,r=100,q=0.1,k=0,initial_value=samples[0])
+f = [kf.update(i) for i in samples]
+f2 = [kf2.update(i) for i in samples]
+plt.plot(x,samples,x,f,x,f2)
 plt.show()
